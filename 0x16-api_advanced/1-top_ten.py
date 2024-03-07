c56e179a93ec4e643515a0fc11276522ce8c6c96
Joeeazy <joeeazy34@gmail.com>
2024-03-07 17:02:57 +0300

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,26 +2,18 @@
 """
 Top Ten
 """
-import requests
-
 
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts listed for a given subreddit.
-    """
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    headers = {"User-Agent": "Python/requests"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """Queries the Reddit API and returns the top 10 hot posts
+    of the subreddit"""
+    import requests
 
-    if response.status_code == 200:
-        try:
-            results = response.json().get("data")
-            [
-                print(c.get("data").get("title"))
-                for c in results.get("children")
-            ]
-
-        except KeyError:
-            return 0
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        print('None')
     else:
-        return 0
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
