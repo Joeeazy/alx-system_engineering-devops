86a8de112a83f657deea21c5318af1c8a56f9103
Joeeazy <joeeazy34@gmail.com>
2024-03-15 01:35:18 +0300

    login 0 errors

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,5 @@
 # This script enables the userholberton to login and open files without error
-i
+
 # Increases the hard file limit for userholberton
 exec { 'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
