8588b57cd9dcb834f8697c3afe123d34fc30bb38
Joeeazy <joeeazy34@gmail.com>
2024-04-20 06:48:32 +0300

    deleted some folders

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# fixes Apache 500 error in wordpress
-exec { 'fix wordpress':
-  onlyif  => 'test -e /var/www/html/wp-settings.php',
-  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
