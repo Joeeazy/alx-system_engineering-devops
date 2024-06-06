edcf3cabc401923404b42da40aae7e72fbfa7539
Joeeazy <joeeazy34@gmail.com>
2024-06-07 01:02:24 +0300

    o-subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,10 +4,11 @@ Script that queries RedditApi and
 returns subscribers on a given Reddit subreddit.
 """
 
-import requests
-
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
+
+    import requests
+    
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
